9906fdcc5f4252010bbf46036ceffb68a62e7ab5
VenkataRamanaMakkaGitHub <129928303+VenkataRamanaMakkaGitHub@users.noreply.github.com>
2023-04-06 11:53:18 +0530

    add file from own files
    array reverse program

---
+++ ArrRev.java
@@ -0,0 +1,32 @@
+/* 24-01-2023 
+ * day 2
+ * assignment qustino
+ */
+package basics_venkat;
+
+import java.util.Scanner;
+
+public class ArrRev {
+
+	public static void main(String args[]) {
+
+		Scanner obj = new Scanner(System.in);
+
+		int arr[] = new int[5];
+
+		System.out.print("Enter 5 values:");
+
+		for (int i = 0; i < arr.length; i++) {
+
+			arr[i] = obj.nextInt();
+		}
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.println(arr[i]);
+		}
+
+		obj.close();
+
+	}
+
+}
